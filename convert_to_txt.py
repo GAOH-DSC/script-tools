7e49266c7f7342b7bc8da4d235f093069b4bcdbf
diskmanjaro <Jorge Andrs Ochoa H>
2022-05-26 09:57:05 -0500

    add comments, convert function to private function

--- convert_to_txt.py
+++ convert_to_txt.py
@@ -1,8 +1,33 @@
+"""
+
+Script use to convert a document and or set of documentos to a txt format
+
+
+"""
+
 import textract
 import os
 import argparse
 
-def convert_source_to_txt(path, output_directory=None):
+def _convert_source_to_txt(path: str, output_directory: str = None) -> None:
+    """
+    Convert any document to a txt format,
+
+    Parameters
+    __________
+    
+    path : string
+       Path to a file or folder.
+    
+    output_directory : string, optional
+            Path to an output folder where save output result.
+
+    Returns
+    _______
+
+    None        
+    """
+
     text = textract.process(path)
     text = text.decode("utf-8")
     full_path = path.split("/")
@@ -19,22 +44,40 @@ def convert_source_to_txt(path, output_directory=None):
             f.write(text)
     
 
-def sources_to_txt(path, output_directory=None):
+def sources_to_txt(path: str, output_directory: str = None) -> None:
+    """
+    Convert a document or a set of documents to a txt format
+
+    Parameters
+    __________
+    
+    path : string
+       Path to a file or folder.
+    
+    output_directory : string, optional
+            Path to an output folder where save output result.
+
+    Returns
+    _______
+
+    None        
+    """
     if os.path.isdir(path):
         list_of_files = os.listdir(path)
         for file in list_of_files:
             name_of_file = (f'{path}/{file}')
             if output_directory is not None:
-                convert_source_to_txt(name_of_file, output_directory)
+                _convert_source_to_txt(name_of_file, output_directory)
             else:
                 convert_source_to_txt(name_of_file)
     if os.path.isfile(path):
         if output_directory is not None:
-            convert_source_to_txt(path, output_directory)
+            _convert_source_to_txt(path, output_directory)
         else:
-            convert_source_to_txt(path)
+            _convert_source_to_txt(path)
 
 def parse_opt():
+    # Function used to receive command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', type=str, help='path of file, can be a directory')
     parser.add_argument('--output-directory', type=str, help='optional path output directory')
@@ -42,6 +85,7 @@ def parse_opt():
     return opt
 
 def main(opt):
+    # Main function
     sources_to_txt(**vars(opt))
 
 
